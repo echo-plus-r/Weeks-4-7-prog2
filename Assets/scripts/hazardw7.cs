e3c73a9c7a8b0f2020f0546d631ea3a006684faa
echo-plus-r <tostesi@shernet.sheridancollege.ca>
2025-02-19 11:55:34 -0500

    added more comments to the hazard script

--- Assets/scripts/hazardw7.cs
+++ Assets/scripts/hazardw7.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class hazardw7 : MonoBehaviour
 {
-    public GameObject player;
-    public bool follow;// holds if the object should follow the player or not
-    public bool move;// holds if the object should move or not
-    public bool loot;// holds if the object is loot or a hazard
-    SpriteRenderer SpriteRenderer;
-    hazardw7 script;
+    public GameObject player; // holds the player, this is used to calculate positions and such
+    public bool follow; // holds if the object should follow the player or not
+    public bool move; // holds if the object should move or not
+    public bool loot; // holds if the object is loot or a hazard
+    SpriteRenderer SpriteRenderer; // holds the sprite render of the object
+    hazardw7 script; // holds a refrence to this script
     // Start is called before the first frame update
     void Start()
     {
+        // getting the related components
         script = GetComponent<hazardw7>();
         SpriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -21,11 +22,11 @@ public class hazardw7 : MonoBehaviour
     void Update()
     {
         Vector2 pos = transform.position;
-        //throw all the collision stuff here or something idk
 
         // this is the worst way to do colisions ever. if you are an aspiring game developer/programer, please don't do this.
         if (pos.x + 0.5f >= player.transform.position.x && pos.x - 0.5f <= player.transform.position.x) 
         {
+            // checking if the object is loot or not
             if (loot == true)
             {
                 Debug.Log("player has grabbed loot");
@@ -36,18 +37,23 @@ public class hazardw7 : MonoBehaviour
                 SpriteRenderer.enabled = false;
                 script.enabled = false;
             }
+
+            // else statement runs if the object is not loot, meaning it's a hazard
             else
             {
                 Debug.Log("player has touched a hazard");
             }
         }
 
+        // if statement for following the player
         if (follow == true) 
         {
+            // if the object's current position is greater than the player's, remove from the x value
             if (pos.x > player.transform.position.x) 
             {
                 pos.x -= 1 * Time.deltaTime;
             }
+            // if the object's current position is less than the player's, add from the x value
             if (pos.x < player.transform.position.x) 
             {
                 pos.x += 1 * Time.deltaTime;
