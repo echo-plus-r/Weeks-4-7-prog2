22d132b9a71e4223e20353298bd9912cdb809057
echo-plus-r <tostesi@shernet.sheridancollege.ca>
2025-02-19 11:52:18 -0500

    added some comments in the hazard script. added two new variables to refrence the object's sprite renderer and the script itself (the variable is a refrence to the script it's in). made it so that loot vanishes when it is collected, and cannot be interacted with or grabbed again.

--- Assets/scripts/hazardw7.cs
+++ Assets/scripts/hazardw7.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class hazardw7 : MonoBehaviour
 {
     public GameObject player;
-    public bool follow;
-    public bool move;
-    public bool loot;
+    public bool follow;// holds if the object should follow the player or not
+    public bool move;// holds if the object should move or not
+    public bool loot;// holds if the object is loot or a hazard
+    SpriteRenderer SpriteRenderer;
+    hazardw7 script;
     // Start is called before the first frame update
     void Start()
     {
-        
+        script = GetComponent<hazardw7>();
+        SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,6 +29,12 @@ public class hazardw7 : MonoBehaviour
             if (loot == true)
             {
                 Debug.Log("player has grabbed loot");
+
+                // making the loot disapear after it is collected
+                // again, this is sort of bad code, don't try this at home.
+                // the better way to do this would be to destroy the object or something
+                SpriteRenderer.enabled = false;
+                script.enabled = false;
             }
             else
             {
