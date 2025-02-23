ed1e2fe022d761dddd43f79594eb079623e33e41
DESKTOP-2933QV0\simon <tostesi@sheridancollege.ca>
2025-02-23 16:04:18 -0500

    added a timer and comments to plant_act script. made the ui a lot nicer. moved the plant that all of the other plants are copied from really far away.

--- Assets/scripts/plant_act.cs
+++ Assets/scripts/plant_act.cs
@@ -11,6 +11,8 @@ public class plant_act : MonoBehaviour
     public Sprite[] sprites; // used to hold sprites the plant can be
     SpriteRenderer sr; // hods the object's sprite renderer
     public TMP_Dropdown dropdown; // holds related dropdown ui element
+    bool og = false;
+    float timer;
 
     // Start is called before the first frame update
     void Start()
@@ -23,17 +25,37 @@ public class plant_act : MonoBehaviour
 
         // setting size as the size from a slider
         transform.localScale = siz;
+
+        // this checks the x position of the object, if it's really far away, it's assumed to be the original plant, and therefore should not destroy itself.
+        if (transform.position.x > 800) 
+        {
+            og = true;
+        }
+
+        // setting the timer to 0
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // adding to the timer if the plant is not the original one
+        if (!og) 
+        {
+            timer += Time.deltaTime;
+        }
+
+        // destroying the plant if the timer is high enough and the plant is not the original one
+        if (timer >= 10 && !og) 
+        {
+            Destroy(gameObject);
+        }
     }
 
     // size is called by a slider.
     public void size(float x) 
     { 
+        // copies the value of the slider into a Vector2 that sets the size of a plant in the start function
         siz.x = x;
         siz.y = x;
     }
